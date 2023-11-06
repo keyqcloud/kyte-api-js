e43a194441634acfd5f27d8f9f0842d6259090c9
Kenneth P. Hough <kenneth@egtech.us>
2023-11-06 17:22:24 -0600

    release 1.0.13

--- kyte-source.js
+++ kyte-source.js
@@ -17,7 +17,7 @@
  **/
 class Kyte {
 	/** KyteJS Version # */
-	static VERSION = '1.0.12';
+	static VERSION = '1.0.13';
 	/** **************** */
 
 	constructor(url, accessKey, identifier, account_number, applicationId = null) {
