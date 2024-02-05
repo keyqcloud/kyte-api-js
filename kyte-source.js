f454449316f32611ef1c3645f92cbf25c8143ed6
Kenneth Hough <kenneth@keyqcloud.com>
2024-02-05 09:55:06 +0900

    Update version

--- kyte-source.js
+++ kyte-source.js
@@ -17,7 +17,7 @@
  **/
 class Kyte {
 	/** KyteJS Version # */
-	static VERSION = '1.1.2';
+	static VERSION = '1.1.3';
 	/** **************** */
 
 	constructor(url, accessKey, identifier, account_number, applicationId = null) {
